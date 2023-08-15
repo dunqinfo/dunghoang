261da013a04a2be2f24e9d71fbc3489dc8524e84
Bit0 <hosybaonhan2004@gmail.com>
2023-08-15 12:18:19 +0700

    updated github and instagram

--- app/data.tsx
+++ app/data.tsx
@@ -1,6 +1,6 @@
 import { ReactElement } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faFacebook, faGithub, faInstagram } from '@fortawesome/free-brands-svg-icons';
 export type information = {
     icon:ReactElement;
     text:string;
@@ -11,5 +11,14 @@ export const data:information[] = [
         icon: <FontAwesomeIcon icon={faFacebook} />,
         text: "Dung Hoàng",
         link_to: "https://www.facebook.com/convocuaoikawa"
+    },{
+        icon: <FontAwesomeIcon icon={faInstagram} />,
+        text: "hdunq201_",
+        link_to: "https://www.instagram.com/hdunq201_"
+    },{
+        icon: <FontAwesomeIcon icon={faGithub} />,
+        text: "hdunq11",
+        link_to: "https://github.com/hdunq11"
     }
+
 ];
